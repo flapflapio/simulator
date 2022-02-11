86242cfbe041b34c62e3ff1362c65daf640dd7bb
Ethan Benabou <ethanbnb@gmail.com>
2022-02-11 12:21:30 -0500

    WIP: Added CDK stack, still need to run and debug it

---
+++ kubernetes/cognito/cdk/lib/cdk-stack.ts
@@ -0,0 +1,103 @@
+import {
+  Stack,
+  StackProps,
+  aws_cognito,
+  RemovalPolicy,
+  CfnOutput,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class FlapFlapCognitoStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // Cognito User Pool
+    const userPool = new aws_cognito.UserPool(this, "flapflap-userpool", {
+      userPoolName: "flapflap-userpool",
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+      autoVerify: { email: true },
+      standardAttributes: {
+        givenName: {
+          required: true,
+          mutable: true,
+        },
+        familyName: {
+          required: true,
+          mutable: true,
+        },
+      },
+      customAttributes: {
+        isAdmin: new aws_cognito.StringAttribute({ mutable: true }),
+      },
+      passwordPolicy: {
+        minLength: 6,
+        requireLowercase: true,
+        requireDigits: true,
+        requireUppercase: false,
+        requireSymbols: false,
+      },
+      accountRecovery: aws_cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
+
+    const cognitoStandardAttributes: aws_cognito.StandardAttributesMask = {
+      givenName: true,
+      familyName: true,
+      email: true,
+      emailVerified: true,
+      address: true,
+      birthdate: true,
+      gender: true,
+      locale: true,
+      middleName: true,
+      fullname: true,
+      nickname: true,
+      phoneNumber: true,
+      phoneNumberVerified: true,
+      profilePicture: true,
+      preferredUsername: true,
+      profilePage: true,
+      timezone: true,
+      lastUpdateTime: true,
+      website: true,
+    };
+
+    // Cognito User Pool Client
+    const userPoolClient = new aws_cognito.UserPoolClient(
+      this,
+      "flapflap-userpool-client",
+      {
+        userPool,
+        userPoolClientName: "flapflap-userpool-client",
+        authFlows: {
+          adminUserPassword: true,
+          custom: true,
+
+          // SRP = Secure Remote Password
+          // See: https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-authentication-flow.html
+          userSrp: true,
+        },
+        supportedIdentityProviders: [
+          aws_cognito.UserPoolClientIdentityProvider.COGNITO,
+        ],
+        readAttributes: new aws_cognito.ClientAttributes()
+          .withStandardAttributes(cognitoStandardAttributes)
+          .withCustomAttributes("isAdmin"),
+        writeAttributes:
+          new aws_cognito.ClientAttributes().withStandardAttributes(
+            cognitoStandardAttributes
+          ),
+      }
+    );
+
+    // OUTPUT: user pool id
+    new CfnOutput(this, "flapflap-userpool-id", { value: userPool.userPoolId });
+
+    // OUTPUT: user pool client id
+    new CfnOutput(this, "flapflap-userpool-client-id", {
+      value: userPoolClient.userPoolClientId,
+    });
+  }
+}
